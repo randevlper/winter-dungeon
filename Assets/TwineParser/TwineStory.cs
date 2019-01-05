bfba9e5d351f07f96d7859468ca6aafd8691b345
Randall Hines <randallhines0@gmail.com>
2019-01-04 23:03:57 -0800

    -Add TextMesh Pro -Whitebox UI

--- Assets/TwineParser/TwineStory.cs
+++ Assets/TwineParser/TwineStory.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public struct TwineStorydata{
+public struct TwineStorydata {
 	public string name;
 	public int startNode;
 	public string creator;
@@ -16,27 +16,59 @@ public struct TwineStorydata{
 	//StyleData
 	public TwinePassage[] passages;
 	public TwineStyle style;
+
+	//Constant Null TwineStoryData
+	public static TwineStorydata Null {
+		get {
+			TwineStorydata retval = new TwineStorydata ();
+			retval.startNode = -1;
+			return retval;
+		}
+	}
+
+	public bool IsNull () {
+		return startNode < 0;
+	}
 }
 
 [System.Serializable]
-public struct TwineStyle
-{
+public struct TwineStyle {
 	public string role;
 	public string id;
 	public string type;
 }
 
 [System.Serializable]
-public struct TwinePassage
-{
+public struct TwinePassage {
 	public Vector2Int position;
 	public Vector2Int size;
 	public string name;
 	public int pid;
 	public string[] tags;
 	public string[] lines;
+
+	public static TwinePassage Null {
+		get {
+			TwinePassage retval = new TwinePassage ();
+			retval.pid = -1;
+			return retval;
+		}
+	}
+
+	public bool IsNull () {
+		return pid < 0;
+	}
 }
 
 public class TwineStory : ScriptableObject {
 	public TwineStorydata storyData;
+
+	public TwinePassage GetPassage (string name) {
+		for (int i = 0; i < storyData.passages.Length; i++) {
+			if (storyData.passages[i].name == name) {
+				return storyData.passages[i];
+			}
+		}
+		return TwinePassage.Null;
+	}
 }
---
+++ Assets/WinterDungeon/Scripts/Dialog/DialogController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogController : MonoBehaviour {
+	[SerializeField] TwineStory story;
+
+	//Scene Items
+	[SerializeField] GameObject dialogBox;
+	[SerializeField] TextMeshProUGUI dialogText;
+
+	[SerializeField] GameObject nameBox;
+	[SerializeField] TextMeshProUGUI nameText;
+
+	[SerializeField] GameObject portraits;
+	[SerializeField] GameObject buttons;
+
+	//Debug items
+	[SerializeField] string startName = "start";
+
+	//Working varibles
+	TwinePassage currentPassage;
+	int currentLine;
+
+	void Awake () {
+		currentPassage = story.GetPassage (startName);
+		NextLine();
+	}
+
+	void Update () {
+		if(Input.GetButtonDown("Fire1")){
+			NextLine();
+		}
+	}
+
+	void NextLine () {
+		dialogText.text = currentPassage.lines[currentLine];
+		currentLine++;
+	}
+}
